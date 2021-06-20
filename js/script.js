dbdd5382c0985596b2dc771b0578e44183b23f7b
Robert Maina <robert.maina@student.moringaschool.com>
2021-06-20 19:06:44 +0300

    used the alert function in jQuery

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     $("#design-icon").click(function(){
         $("#toggle-design").toggle();
     });
+
+    $("form").submit(function(){
+      var name= $("#names").val();
+      alert(name + " we have received your message. Thank you for reaching out to u");
+    });
        
 });
 
